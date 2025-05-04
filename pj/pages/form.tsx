67b06dbee656e062460d6d4db4d9d6bc40e82bc7
haru-oss <hauyaishungry@gmail.com>
2025-05-04 14:24:14 +0900

    submitのセットアップをした。この中に処理を書く

--- pj/pages/form.tsx
+++ pj/pages/form.tsx
@@ -6,59 +6,62 @@ import "react-datepicker/dist/react-datepicker.css"
 export default function Home() {
   const [startDate, setStartDate] = useState(new Date());
 
+  const handleSubmit = ()=>{};
+
   return (
 
     <Box maxW="600px" mx="auto" mt={10} p={6} borderWidth={1} borderRadius="lg" boxShadow="md">
         <Button colorScheme='teal' >
             <Link href="/">Home</Link>
-            </Button>
-      <form>
+       </Button>
+
+      <form onSubmit={handleSubmit}>
         <VStack spacing={3} align="stretch">
-          <FormControl isRequired>
-            <FormLabel>投入日</FormLabel>
-            <Box
-              border="1px"
-              borderColor="gray.200"
-              borderRadius="md"
-              p={2}
-              mb={2}
-            >
-            <DatePicker
-              selected={startDate}
-              onChange={(date) => setStartDate(date as Date)}
-              dateFormat="yyyy/MM/dd"
-              className="chakra-input css-1c6u84y"
-            />
+            <FormControl isRequired>
+              <FormLabel>投入日</FormLabel>
+              <Box
+                border="1px"
+                borderColor="gray.200"
+                borderRadius="md"
+                p={2}
+                mb={2}
+              >
+              <DatePicker
+                selected={startDate}
+                onChange={(date) => setStartDate(date as Date)}
+                dateFormat="yyyy/MM/dd"
+                className="chakra-input css-1c6u84y"
+              />
 
-            </Box>
-            <FormLabel>店名</FormLabel>
-            <Input />
-          </FormControl>
+              </Box>
+              <FormLabel>店名</FormLabel>
+              <Input />
+            </FormControl>
 
-          <FormControl isRequired>
-            <FormLabel>決済者</FormLabel>
-            <Input  />
-          </FormControl>
+            <FormControl isRequired>
+              <FormLabel>決済者</FormLabel>
+              <Input  />
+            </FormControl>
 
-          <FormControl>
-            <FormLabel>他社情報、会えた人、決済者の出勤日等</FormLabel>
-            <Textarea  />
-          </FormControl>
+            <FormControl>
+              <FormLabel>他社情報、会えた人、決済者の出勤日等</FormLabel>
+              <Textarea  />
+            </FormControl>
 
-          <FormControl>
-            <FormLabel>商品記号など</FormLabel>
-            <Input  />
-          </FormControl>
+            <FormControl>
+              <FormLabel>商品記号など</FormLabel>
+              <Input  />
+            </FormControl>
 
 
-          <FormControl>
-            <FormLabel>場所</FormLabel>
-            <Input placeholder='場所を入力してください' />
-          </FormControl>
+            <FormControl>
+              <FormLabel>場所</FormLabel>
+              <Input placeholder='場所を入力してください' />
+            </FormControl>
 
-          <Button colorScheme="blue" size="lg" type="submit" mt={4}>
-            送信
-          </Button>
+            <Button colorScheme="blue" size="lg" mt={4} type="submit">
+              送信
+            </Button>
 
         </VStack>
       </form>
