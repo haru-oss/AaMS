f50c8e54b4b412ed36dc2be8eb45c5610c0bd6c8
haru-oss <hauyaishungry@gmail.com>
2025-05-05 19:34:11 +0900

    謎のエラー

--- pj/pages/api/save.ts
+++ pj/pages/api/save.ts
@@ -6,7 +6,8 @@ const prisma = new PrismaClient()
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Method not allowed' })
+res.status(405).json({ message: 'Method not allowed' })
+return;
   }
 
   try {
