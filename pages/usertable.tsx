e86068ee2db451065f0b013bd56e97b6d6f917b5
haru-oss <hauyaishungry@gmail.com>
2025-05-18 10:50:57 +0900

    この差分をmergeしてみる

--- pages/usertable.tsx
+++ pages/usertable.tsx
@@ -112,38 +112,7 @@ export default function UserTable () {
 
 
            </>
-/*
-       <Box maxW="90%" mx="auto" mt={10}>
-      <Heading mb={6}>顧客一覧</Heading>
-      <Table variant="simple" size="md">
-        <Thead>
-          <Tr>
-            <Th>投入日</Th>
-            <Th>店名</Th>
-            <Th>決済者</Th>
-            <Th>メモ</Th>
-            <Th>商品記号</Th>
-            <Th>場所</Th>
-            <Th>回収</Th>
-            <Th>契約</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {users.map((user) => (
-            <Tr key={user.id}>
-              <Td>{new Date(user.MonitordAt).toLocaleDateString()}</Td>
-              <Td>{user.storeName}</Td>
-              <Td>{user.decisionMaker}</Td>
-              <Td>{user.notes}</Td>
-              <Td>{user.productCode}</Td>
-              <Td>{user.location}</Td>
-              <Td> <Button>回収</Button></Td>
-              <Td> <Button>契約</Button></Td>
-            </Tr>
-          ))}
-        </Tbody>
-      </Table>
-    </Box> */
+
   );
 }
 
